6b4a64849843ec6dcaaadb0f949b23eb90d3f7c8
balasurya96 <114472977+balasurya96@users.noreply.github.com>
2022-09-27 15:40:12 +0530

    Add files via upload

---
+++ balasuriyatest5.py
@@ -0,0 +1,8 @@
+Db={
+    "Parttime":
+        {"Order":25,"pertol":50,"allowance":75,},
+    "Fulltime":
+        {"Order": 30,"pertol": 150,"allowance":180,},
+    "Weekend":
+        {"Order": 35,"pertol": 150,"allowance":250,}
+    }
